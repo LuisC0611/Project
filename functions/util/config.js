0ed0dc1271ee749060cc28911610d53fd1450b75
LuisC0611 <cruzmendoza6@gmail.com>
2023-05-01 01:20:56 +0000

    New Origin

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCFkOFKhOVuxRyQFNeOGZHTHZMdDIOwWwQ",
+    authDomain: "todoapp-fbc41.firebaseapp.com",
+    databaseURL: "........",
+    projectId: "todoapp-fbc41",
+    storageBucket: "todoapp-fbc41.appspot.com",
+    messagingSenderId: "108291263956",
+    appId: "1:108291263956:web:dd6a454dc60d6f82ba9a41",
+    measurementId: "G-E39WWK8ZZ5"
+};
